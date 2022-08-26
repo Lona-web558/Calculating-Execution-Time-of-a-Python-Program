77b243eb7ce2d1eb60e5d9bc7a721bf177413f97
Lona Matshingana <71089848+Lona-web558@users.noreply.github.com>
2022-08-26 13:23:51 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+#Execution Time of a Python Program
+
+from time import time
+start = time()
+
+#Python program to create acronyms
+
+word = "Artificial Intelligence"
+text = word.split()
+a = ""
+for i in text:
+    a = a+str(i[0].upper())
+print(a)
+
+end = time()
+execution_time = end
+print("Execution Time:", execution_time)
